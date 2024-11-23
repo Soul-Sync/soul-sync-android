6b5f0aae229e35b69ffe9e285206ecb85f434c91
azizahfauni <azizahfs30@gmail.com>
2024-11-23 23:30:42 +0700

    layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -67,8 +67,8 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     ksp("com.github.bumptech.glide:compiler:4.16.0")
 
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 
 
 }
--- app/src/main/java/com/dicoding/soulsync/start/StartActivity.kt
+++ app/src/main/java/com/dicoding/soulsync/start/StartActivity.kt
@@ -1,7 +1,10 @@
 package com.dicoding.soulsync.start
 
+
+import android.animation.ObjectAnimator
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.soulsync.MainActivity
@@ -14,6 +17,16 @@ class StartActivity : AppCompatActivity() {
         setContentView(R.layout.activity_start)
 
         val startButton: Button = findViewById(R.id.startButton)
+        val appImageView = findViewById<View>(R.id.appImageView)
+
+        // Animasi gambar bergerak ke kiri dan kanan dengan efek berulang
+        val imageAnimator = ObjectAnimator.ofFloat(appImageView, View.TRANSLATION_X, -30f, 30f).apply {
+            duration = 6000  // Durasi animasi 6 detik
+            repeatCount = ObjectAnimator.INFINITE  // Ulang terus-menerus
+            repeatMode = ObjectAnimator.REVERSE  // Kembali ke posisi awal setelah gerakan
+        }
+        imageAnimator.start()
+
         startButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
